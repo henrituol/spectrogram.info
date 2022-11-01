545abd3afb973c85f32c33949b814954325a56e2
Henri Pitkänen <henupitkanen@gmail.com>
2022-11-01 13:34:11 +0200

    Updated styles and mobile view

--- html/spectroquiz.js
+++ html/spectroquiz.js
@@ -16,12 +16,15 @@ let dataFromXenocanto;
 // The scope of recordings is confined to A quality recordings.
 fetch("https://xeno-canto.org/api/2/recordings?query=q:A") // xeno-canto query into a promise
 .then(response => response.json())  // Turn it into object
-.then(utilizeFetchedData)           // Take the data to good use. See function below.
+.then(saveData)           // Take the data to good use. See function below.
 .catch(console.error);
 
-function utilizeFetchedData(data) {
-    
+function saveData(data) {
     dataFromXenocanto = data; // Save data into the global variable for later use.
+    utilizeFetchedData(dataFromXenocanto); // This is also called later, hence, this different function; no need to save data again.
+}
+
+function utilizeFetchedData(data) {
     
     // There are 500 recordings in the first page of data, therefore, let's pick one randomly between 1-500.
     let randomNumber = Math.floor(Math.random() * 501);
@@ -38,7 +41,7 @@ function utilizeFetchedData(data) {
         // Should be quite unlikely that we ever got here, let alone stay here for long.
         while (randomNumber == secondRandomNumber) {
             secondRandomNumber = Math.floor(Math.random() * 501);
-            console.log("We ended up making a new random number!")
+            //console.log("We ended up making a new random number!")
         }
     }
     let firstFalse = (data.recordings[secondRandomNumber]);
@@ -49,7 +52,7 @@ function utilizeFetchedData(data) {
     if (randomNumber == thirdRandomNumber) {
         while (randomNumber == thirdRandomNumber) {
             thirdRandomNumber = Math.floor(Math.random() * 501);
-            console.log("We ended up making a new random number!")
+            //console.log("We ended up making a new random number!")
         }
     }
     let secondFalse = (data.recordings[thirdRandomNumber]);
@@ -74,7 +77,6 @@ function randomizeSpectrograms () {
     // However, many also note "not all permutations have the same probability"
     // https://javascript.info/task/shuffle 
     // Then again, in this context, it is sufficient.
-    //  -- Or not! Seems like option C is preferred at the beginning.
     function shuffle(array) {
         array.sort(() => Math.random() - 0.5);
       }
@@ -118,17 +120,22 @@ function createQuizPage(audioFile, image1, image2, image3, whichOneIsRight) {
     // Change welcome text into instructions.
     document.getElementById("firstParagraph").innerHTML="Listen to the audio sample and select the corresponding spectrogram.";
 
+    document.querySelector(".firstrow").style.background = "aliceblue";
+    document.querySelector(".secondrow").style.background = "aliceblue";
+    document.querySelector(".thirdrow").style.background = "aliceblue";
+
     // Also, at start of function, empty divs. 
     // Otherwise, content will stack up when button is pushed.
     clearDivs();
 
-    // Select song from the local folder
+    // Select song from the local folder and create the player.
     createPlayer(audioFile);
 
     // Display spectrograms and checkboxes
     displaySpectrogram(image1, 1); // Display spectrogram specified when function is called on the row #1.
     displaySpectrogram(image2, 2);
     displaySpectrogram(image3, 3);
+    document.getElementById("result").innerHTML= "Select a checkbox."
 
     // Create checkbox listeners 
     document.getElementById("A").addEventListener(
@@ -152,20 +159,64 @@ function createQuizPage(audioFile, image1, image2, image3, whichOneIsRight) {
     function selectionResult(checkboxId, whichOneIsRight) {
         if (checkboxId == whichOneIsRight) {
             document.getElementById("result").innerHTML="Correct answer!";
+            switch(checkboxId) {
+                case "A":
+                    document.querySelector(".firstrow").style.background = 'palegreen';
+                    break;
+                case "B":
+                    document.querySelector(".secondrow").style.background = 'palegreen';
+                    break;
+                case "C":
+                    document.querySelector(".thirdrow").style.background = 'palegreen';
+                    break;
+            }
         } else {
-            let answer = "You chose " + checkboxId + ". " + "The correct answer is " + whichOneIsRight + "."; 
+            let answer = "Wrong answer!"; 
             document.getElementById("result").innerHTML=answer;
+            switch(checkboxId) {
+                case "A":
+                    document.querySelector(".firstrow").style.background = 'salmon';
+                    break;
+                case "B":
+                    document.querySelector(".secondrow").style.background = 'salmon';
+                    break;
+                case "C":
+                    document.querySelector(".thirdrow").style.background = 'salmon';
+                    break;
+            }
         }
     }
 
-    // Check, if there is already a selection symbol in another changebox.
+    // Check, if there is already a selection symbol in another checkbox.
     // If there is, clear that box. Otherwise, it will look like you could have multiple choices, which isn't true.
     function uncheckPreviousAnswer(anotherCheckbox, yetAnotherCheckbox) {
         if (document.getElementById(anotherCheckbox).checked == true) {
             document.getElementById(anotherCheckbox).checked = false;
+            switch(anotherCheckbox) {
+                case "A":
+                    document.querySelector(".firstrow").style.background = 'aliceblue';
+                    break;
+                case "B":
+                    document.querySelector(".secondrow").style.background = 'aliceblue';
+                    break;
+                case "C":
+                    document.querySelector(".thirdrow").style.background = 'aliceblue';
+                    break;
+            }
         } 
         if (document.getElementById(yetAnotherCheckbox).checked == true) {
             document.getElementById(yetAnotherCheckbox).checked = false;
+            switch(yetAnotherCheckbox) {
+                case "A":
+                    document.querySelector(".firstrow").style.background = 'aliceblue';
+                    break;
+                case "B":
+                    document.querySelector(".secondrow").style.background = 'aliceblue';
+                    break;
+                case "C":
+                    document.querySelector(".thirdrow").style.background = 'aliceblue';
+                    break;
+            }
         }
     }
 }
@@ -182,6 +233,7 @@ function createPlayer(songFile) {
     song.src = songFile;
     song.type = "audio/mp3";
     document.querySelector(".player").appendChild(song);
+    // song.play(); // If this is enabled, the console says: "Uncaught (in promise) DOMException: The fetching process for the media resource was aborted by the user agent at the user's request."
 }
 
 // Display three spectrograms and create checkboxes next to the images.
@@ -190,7 +242,15 @@ function displaySpectrogram(spectrogramFile, numberOfRow) {
     // Create spectrogram related variable and information
     const image = document.createElement("img");
     image.src = spectrogramFile;
-    image.width = 500;
+    // Image width depending on the size of the viewport, i.e., desktop or mobile.
+    let maxWidth = document.documentElement.clientWidth;
+    //console.log(maxWidth);
+    if (maxWidth < 600) {
+        image.width = maxWidth * 0.75;
+    } else {
+        image.width = 500;
+    }
+    
 
     // Create checkbox related variable and information
     let checkbox = document.createElement("input");
